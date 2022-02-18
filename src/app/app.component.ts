f6086e00137717c03e984a4ac3280d35a77aad10
Christoph Höller <christoph.hoeller@gmail.com>
2022-02-18 15:00:13 +0100

    implement button-chooser-client app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'button-chooser-client';
+  choices = ['Mono-Repo', 'NPM Library'];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ButtonChooserModule } from 'ch-button-chooser';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonChooserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
